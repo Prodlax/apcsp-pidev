7471dc7676fcfd6a2ca29746dfd2039d3dea54f5
Prodlax <derekma02@gmail.com>
2019-05-23 15:33:56 +0000

    size of variables

---
+++ cdemo/sizeofvariables.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+  int a = 545;
+  char b = 'c';
+  float c = 99.998;
+  double d = 13.421;
+
+  // print value and size of an int variable
+  printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
+  printf("int a value: %d and size: %d bytes\n", b, sizeof(b));
+  printf("int a value: %d and size: %d bytes\n", c, sizeof(c));
+  printf("int a value: %d and size: %d bytes\n", d, sizeof(d)); 
+}
+
+
+//{
+//  char a = c;
+//
+  // print value and size of an int variable
+ // printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
+//}
+
+
+
+//{
+ 
