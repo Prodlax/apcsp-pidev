f91ead40d455e892641cc3e84bfc09ee7ba2d3a9
Prodlax <derekma02@gmail.com>
2019-05-28 21:24:03 +0000

    areaOfCircle2

--- cdemo/areaOfCircle2.c
+++ cdemo/areaOfCircle2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-
+#include <stdlib.h>
 
 float areaOfCircle(float r)
 {
@@ -12,12 +12,24 @@ return(a);
 int main()
 {
 float i;
-printf("enter radius: ");
+float j;
+printf("enter lower value: ");
 scanf("%f", &i);
+printf("enter higher value: ");
+scanf("%f", &j);
 
+if(i > j)
 {
-float poo = areaOfCircle(i);
-printf ("The area is %f if the radius is %f\n", poo, i);
+printf("Please enter the lower and higher value correspondingly. Try Again. The program will now terminate\n");
 }
 
+
+for (float h = i; h < j; h+=0.1000000){
+float poo = areaOfCircle(h);
+printf ("The area is %f if the radius is %f\n", poo, h);
+
 }
+
+
+}
+
