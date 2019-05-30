60449b6aecff851d3e59b518e009bad0148f07c7
Prodlax <50209414+Prodlax@users.noreply.github.com>
2019-05-29 22:45:51 -0700

    Update student.h

--- cdemo/myproject/student.h
+++ cdemo/myproject/student.h
@@ -6,13 +6,7 @@ int studentID;
 };
 
 
-void printStudentByPtr(struct Student* s)
-{
-printf("Student -\n");
-printf("  Firstname:%s\n", s->firstname);
-printf("  Lastname:%s\n", s->lastname);
-printf("  Age:%d\n", s->age);
-printf("  id:%d\n", s->studentID);
-}
+void printStudentByPtr(struct Student* s);
+
 
 
